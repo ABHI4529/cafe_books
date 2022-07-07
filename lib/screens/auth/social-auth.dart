562242a39b862c91eca7bd3605677d2fbc28d5d3
ABHI4529 <abhinavgadekar4529@gmail.com>
2022-07-07 15:53:37 +0530

    socail

--- lib/screens/auth/social-auth.dart
+++ lib/screens/auth/social-auth.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart' show kIsWeb;
